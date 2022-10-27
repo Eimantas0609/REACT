c06d113c01d15c9d6e1daf8f5e1e43775744adbe
Eimantas0609 <100865215+Eimantas0609@users.noreply.github.com>
2022-10-27 16:34:22 +0300

    more for fun

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+const cors = require('cors');
+app.use(cors());
+const mysql = require('mysql');
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.json());
+
+const con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'reactCourse',
+});
+
+app.get('/', (req, res) => {
+	res.send('Labas, Briedi!');
+});
+
+app.get('/labas', (req, res) => {
+	const sql = `
+	SELECT id
+	FROM things
+	`;
+	con.query(sql, (err, result) => {
+		if (err) throw err;
+		res.send(result);
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Bebras klauso ${port} porto`);
+});
